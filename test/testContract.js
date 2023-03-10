5718b7cd8db652da23093a8bc71754b46593b90b
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2023-03-10 18:29:18 +0530

    Updated tests

--- test/testContract.js
+++ test/testContract.js
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Addition", function () {
     it("x should be greater then y", async function () {
